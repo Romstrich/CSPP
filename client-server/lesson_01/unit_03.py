6ad653e0aa50acedf2ff9aae2be30804abf3a8f7
Romstrich <roman.motrich@yandex.ru>
2022-05-17 15:55:29 +0300

    война с pip

--- client-server/lesson_01/unit_03.py
+++ client-server/lesson_01/unit_03.py
@@ -1,6 +1,15 @@
 '''3. Определить, какие из слов «attribute», «класс», «функция», «type» невоз
 можно записать в байтовом типе.'''
 
-word1 = 'attribute'
-word2 = 'класс'
-word3 = 'функция'
+words = ['attribute','класс','функция']
+
+for word in words:
+    print(f'{word}\t{type(word)}')
+    try:
+        print(bytes(word,'ascii'))
+    except BaseException as e:
+        print('Ошибка:',e)
+        print(f'{"*"*10}{word} не подходит для прямого перевода в байты')
+
+        
+
---
+++ client-server/lesson_01/unit_05.py
@@ -0,0 +1,13 @@
+'''Выполнить пинг веб-ресурсов yandex.ru, youtube.com
+и преобразовать результаты из байтовового в строковый тип на кириллице.'''
+
+import subprocess
+import chardet
+
+ARGS = ['ping', 'yandex.ru']
+YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
+for line in YA_PING.stdout:
+    result = chardet.detect(line)
+    print(result)
+    line = line.decode(result['encoding']).encode('utf-8')
+    print(line.decode('utf-8'))
