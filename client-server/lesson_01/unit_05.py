c73fd7f185b8f890c390a822d8a49f3ef4c2a9b4
Romstrich <roman.motrich@yandex.ru>
2022-05-19 05:54:55 +0300

    ping

--- client-server/lesson_01/unit_05.py
+++ client-server/lesson_01/unit_05.py
@@ -4,9 +4,9 @@
 import subprocess
 import chardet
 
-ARGS = ['ping', 'yandex.ru']
-YA_PING = subprocess.Popen(ARGS, stdout=subprocess.PIPE)
-for line in YA_PING.stdout:
+my_ping = subprocess.Popen(["/bin/ping", "-c4", "yandex.ru"], stdout=subprocess.PIPE)
+
+for line in my_ping.stdout:
     result = chardet.detect(line)
     print(result)
     line = line.decode(result['encoding']).encode('utf-8')
