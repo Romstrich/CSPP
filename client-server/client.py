6638cf45129861b849cfca2486c38cf6de31978a
Romstrich <roman.motrich@yandex.ru>
2022-05-26 16:33:38 +0300

    main

--- client-server/client.py
+++ client-server/client.py
@@ -9,4 +9,9 @@ import time
 #модуль с готовыми заголовками протокола
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
     RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
-from common.utils import get_message, send_message
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
