56de79ad1689a1c8bd38cb326450c6804a9ec00c
Romstrich <roman.motrich@yandex.ru>
2022-05-26 16:29:40 +0300

    Как начать....

---
+++ client-server/server.py
@@ -0,0 +1,11 @@
+'''
+Сервер
+'''
+import sys
+import json
+import socket
+import time
+#модуль с готовыми заголовками протокола
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
