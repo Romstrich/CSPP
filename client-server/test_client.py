c294796a8477c74cd8135080c01881a5956f4a01
Romstrich <roman.motrich@yandex.ru>
2022-06-04 15:05:32 +0300

    go

--- client-server/test_client.py
+++ client-server/test_client.py
@@ -1,12 +1,31 @@
 import unittest
+from client import create_presence
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME
+import  time
 
-print('import umittest')
-def somesum(a,b):
-    return a+b
+print('import unittest')
+
+
+def somesum(a, b):
+    return a + b
+
 
 class TestFirstTest(unittest.TestCase):
-    def testequal(self):
-        self.assertEqual(somesum(2,5),7)
+    def test_Equal(self):
+        self.assertEqual(somesum(2, 5), 7)
 
-if __name__=='__main__':
+    def test_NoEqual(self):
+        self.assertNotEqual(somesum(2, 5), 6)
+
+    def test_create_presence1(self):
+        self.assertEqual(create_presence(), {ACTION: PRESENCE, TIME: time.time()
+            , USER: {ACCOUNT_NAME: 'Guest', }})#разница во времени-FAILED
+
+    def test_create_presence(self):
+        s_msage=create_presence()
+        time_now=s_msage[TIME]
+        self.assertEqual(s_msage, {ACTION: PRESENCE, TIME:time_now , USER: {ACCOUNT_NAME: 'Guest', }})
+
+    
+if __name__ == '__main__':
     unittest.main()
