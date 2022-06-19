b9f12ac31cd13866f837e4062cafdb28415decba
Romstrich <roman.motrich@yandex.ru>
2022-06-19 09:38:48 +0300

    Есть логирование из декоратора

--- cli-serv/common/decors.py
+++ cli-serv/common/decors.py
@@ -2,6 +2,8 @@
 "<дата-время> Функция func_z() вызвана из функции main"'''
 import sys
 import logging
+import traceback
+import inspect
 
 logging.basicConfig(filename = "log/CSApp.log",format = "%(asctime)s %(levelname)-10s %(module)s %(message)s",level = logging.INFO)
 
@@ -21,8 +23,13 @@ def log(func_to_log):
     #функция-обёртка(сам декоратор)
     def log_saver(*args,**kwargs):
         print('обернул и вернул')
-        LOGGER.info('сообщение')
+        LOGGER.info(f'--- {LOGGER.name} ---' 
+                    f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
+                    f'Вызов из модуля {func_to_log.__module__}. Вызов из'
+                    f' функции {traceback.format_stack()[0].strip().split()[-1]}.'
+                    f'Вызов из функции {inspect.stack()[1][3]}')
         dec_func=func_to_log(*args,**kwargs)
+        # print(LOGGER.name)
         return dec_func
     return log_saver
 
