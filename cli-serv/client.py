ca2b063d86efdb544c842f086b02927408e50763
Romstrich <roman.motrich@yandex.ru>
2022-06-23 11:13:29 +0300

    3

--- cli-serv/client.py
+++ cli-serv/client.py
@@ -73,6 +73,7 @@ def main():
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # заводим сокет
     transport.connect((server_address, server_port))  # стучим
     message_to_server = create_presence()  # тут надо сконструировать
+    print(message_to_server)
     send_message(transport, message_to_server)  # отправить
     try:
         answer = process_ans(get_message(transport))  # принять ответ
--- cli-serv/server.py
+++ cli-serv/server.py
@@ -19,7 +19,7 @@ import select
 import time
 #модуль с готовыми заголовками протокола
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-    RESPONSE,MAX_CONNECTIONS, ERROR,  DEFAULT_PORT
+    RESPONSE,MAX_CONNECTIONS, ERROR,  DEFAULT_PORT,MESSAGE,MESSAGE_TEXT,SENDER
 from common.utils import send_message,get_message
 #модуль с декоратором
 from common.decors import *
@@ -29,19 +29,25 @@ logging.basicConfig(filename = "log/CSApp.log",format = "%(asctime)s %(levelname
 LOGGER = logging.getLogger('server_logger')
 
 @log
-def process_client_message(message):
-    if ACTION in message and message[ACTION]==PRESENCE and TIME in message and USER in message\
-            and message[USER][ACCOUNT_NAME]=='Guest':# если базовые вещи на месте
-        LOGGER.info('нормальное сообщение от клиента')
-        return {RESPONSE:200}
+def process_client_message(message, messages_list, client):
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
+            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+        send_message(client, {RESPONSE: 200})
+        return
+        # Если это сообщение, то добавляем его в очередь сообщений. Ответ не требуется.
+    elif ACTION in message and message[ACTION] == MESSAGE and \
+            TIME in message and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        return
+        # Иначе отдаём Bad request
     else:
-        LOGGER.error('ошибочное сообщение от клиента')
-        return {
-            RESPONSE:400,
-            ERROR:'Request for lesson 3 is not correct'
-        }
+        send_message(client, {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        })
+        return
 
-    return {"some response":"0"}
+
 #server.py -a 127.0.0.1 -p 3039
 #Какой ip слушаем -a
 #Какой порт занимаем -p
@@ -110,9 +116,27 @@ def main():
             for client_with_message in recv_data_lst:
                 try:
                     process_client_message(get_message(client_with_message),messages, client_with_message)
+
                 except:
                     print(f'Клиент {client_with_message.getpeername()} отключился от сервера.')
                     clients.remove(client_with_message)
+
+        if messages and send_data_lst:
+            message = {
+                ACTION: MESSAGE,
+                SENDER: messages[0][0],
+                TIME: time.time(),
+                MESSAGE_TEXT: messages[0][1]
+            }
+            del messages[0]
+            for waiting_client in send_data_lst:
+                try:
+                    print(message)
+                    send_message(waiting_client, message)
+                except:
+                    LOGGER.info(f'Клиент {waiting_client.getpeername()} отключился от сервера.')
+                    clients.remove(waiting_client)
+
         # client, client_address = transport.accept()
         # try:
         #     message_from_client = get_message(client)
